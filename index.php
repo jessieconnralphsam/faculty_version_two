dfb9a89bcd736d07039e1c3c7e52e419a87d6a3f
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-04-04 13:10:53 +0800

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include('includes/header.php'); ?>
-<body>
+<body style="background-color:#F5F7FA;">
   <?php include('includes/topbar.php'); ?>
   <div class="f-container mb-4"></div>
   <div class="bg-f-container">
@@ -12,42 +12,101 @@
     </div>
   </div>
   <div class="container">
-    <div class="row">
-      <div class="col">
-        <div class="input-group">
-          <input type="text" class="form-control" placeholder="Search name" aria-label="Recipient's username" aria-describedby="">
-          <button class="btn border maroon-button" type="button" id=""><i class="fa fa-search color-white"></i></button>
-        </div>
-      </div>
-      <div class="col"></div>
-      <div class="col">
+      <div class="container">
         <div class="row">
-          <div class="col">
-            <div class="row">
-              <div class="col col-md-8">
-                <strong>Show status</strong>
-              </div>
-              <div class="col col-md-2">
-                <input class="form-check-input" type="checkbox" value="" aria-label="">
+            <div class="col-md-7">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search name" aria-label="Recipient's username" aria-describedby="">
+                    <button class="btn maroon-button" type="button" id=""><i class="fa fa-search color-white"></i></button>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="row">
+                    <div class="col text-center">
+                        <div class="row mt-2">
+                            <div class="col">
+                                <strong>Show Status</strong>
+                            </div>
+                            <div class="col-2">
+                                <input class="form-check-input" type="checkbox" value="" aria-label="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col text-center">
+                        <div class="row mt-2">
+                            <div class="col">
+                                <strong>Show Rank</strong>
+                            </div>
+                            <div class="col col-2">
+                                <input class="form-check-input" type="checkbox" value="" aria-label="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      <!-- <div class="container mt-3">
+        <div class="row row-cols-auto">
+          <div class="col px-2 py-2">
+            <div  class="container bg-white rounded">
+              <div class="container">
+                <div class="row">
+                  <div class="col col-md-10">
+                    <div>
+                      <strong>CNSM Faculty Rank</strong>
+                    </div>
+                  </div>
+                  <div class="col col-md-2">
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div class="col">
-            <div class="row">
-              <div class="col col-md-8">
-                <strong>Show status</strong>
-              </div>
-              <div class="col col-md-2">
-                <input class="form-check-input" type="checkbox" value="" aria-label="">
+          <div class="col px-2 py-2">
+            <div  class="container bg-white rounded">
+              <div class="container">
+                <div class="row">
+                  <div class="col col-md-10">
+                    <div>
+                      <strong>CNSM Faculty Rank</strong>
+                    </div>
+                  </div>
+                  <div class="col col-md-2">
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </div> -->
+      <div class="container  py-2 px-2">
+        <div class="container rounded bg-white">
+            <!-- <div class="px-3 py-3 bg-white">
+              <strong>All Colleges / <span class="maroon">College of Natural Sciences and Mathematics (CNSM)</span></strong>
+            </div> -->
+            <div class="px-3 py-3">
+              <strong>All Colleges (10)</strong>
+              <div class="py-4">
+                <div class="row">
+                      <div class="col py-2">
+                          <div class="container py-2 bg-white rounded custom-container border">
+                            <img src="forms/assets/img/65dbdd2cc61cb_ACAPULCO, JUNIVEN P _MG_9332.jpg" class="rounded img-fluid" alt="...">
+                            <h6 class="text-center mt-2 maroon"><strong>College of Business Administration and Accountancy</strong></h6>
+                            <div class="container" style="display: flex; justify-content: center;">
+                                <div style="width: 30%;">
+                                    <hr style="width: 100%; border: 1px solid;">
+                                </div>
+                            </div>
+                            <h6 class="text-center"><strong>Marissa G. Dela Cruz</strong></h6>
+                            <h6 class="text-center">Dean of CBA&A</h6>
+                          </div>
+                      </div>
+                </div>
+              </div>
+            </div>
+        </div>
       </div>
-      <div class="container mt-4">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione minima deleniti dolorem distinctio iste eius minus possimus, quidem autem voluptatibus sequi, totam corporis natus odit, aperiam nostrum nesciunt tenetur quo. Eum facere debitis sint. Laudantium, modi voluptas ullam consequuntur fuga maiores perferendis magni hic. Odio aliquid vitae molestias rerum perferendis incidunt amet, libero recusandae quisquam. Perspiciatis excepturi tempore, praesentium iste sit, sunt eum eius perferendis voluptatem voluptate blanditiis assumenda rem unde, expedita facilis nesciunt asperiores. Officiis quam illum obcaecati quibusdam cumque debitis error molestiae reiciendis enim consequatur! Eligendi animi accusamus quae repudiandae soluta non consequatur quo, recusandae repellat nihil ullam delectus tempore doloremque fugiat cum architecto, vero veniam placeat, dicta amet dolorum iste est! Quo dicta mollitia voluptatem obcaecati sint corrupti maxime doloribus nostrum hic in deserunt magnam aliquid harum quasi voluptatibus, laboriosam officiis adipisci. Expedita asperiores alias, voluptatibus eos ipsa ducimus, soluta illum earum odio magnam qui at esse sapiente aspernatur id beatae ipsam quis? Quaerat iure repellendus harum provident, consectetur tempore enim ab delectus id vel voluptatibus ea perspiciatis possimus exercitationem molestias! Alias iure corporis dolorum, quis culpa esse! Quae debitis quam deleniti maiores recusandae quis dolorum, deserunt praesentium unde officia accusamus vel excepturi rerum delectus minus nemo corrupti. Consectetur possimus quia at reprehenderit inventore ab dicta, facere fugit reiciendis, alias, magnam voluptas? Iste voluptate minus tempora sapiente a recusandae voluptates dolore sequi aliquam veritatis. Harum amet temporibus alias voluptate adipisci minima, expedita deserunt corporis quo voluptatem vel! Neque, tempora fuga molestiae quibusdam maxime magnam ratione reiciendis delectus. Repellat reiciendis quae rem laboriosam consequatur. Dignissimos voluptatum sint vero beatae ipsa aliquam soluta ut accusantium odit quod explicabo saepe magnam fugit pariatur omnis, quis quidem odio cumque eum labore assumenda aut atque consectetur hic! Est omnis quia harum fuga culpa? At unde illo delectus libero consequuntur iure! Velit enim recusandae neque quod sunt. Illum necessitatibus in expedita sequi sed, omnis dolorum nihil. Eius officiis repudiandae dolor est, quos nisi vitae iusto, repellat totam, ex eum accusamus odit labore sed. Obcaecati ducimus fugit, accusamus nisi laborum mollitia optio natus laboriosam ipsam laudantium? Molestiae iste nobis autem animi qui nisi nesciunt quisquam nulla, nemo iusto hic similique magnam tenetur, obcaecati eligendi delectus. Labore impedit quo asperiores! Magni, dignissimos? Ipsam excepturi in impedit amet consectetur blanditiis, aperiam ratione iure alias nisi fuga asperiores dignissimos hic repellendus est eligendi, earum repellat assumenda! Temporibus accusamus ullam deleniti illum, consequatur culpa eaque error iusto ipsum dolores modi, aspernatur voluptatibus ipsam cupiditate rerum officiis. Optio consequatur esse illum a qui omnis sapiente culpa est velit veritatis aut dignissimos, quis repellat cum totam nihil modi accusamus id. Expedita adipisci, officiis odio fugit, consequatur deleniti debitis illum, voluptates odit temporibus soluta optio numquam. Libero, eveniet accusantium? Voluptate ducimus laborum fuga fugiat ipsa sit sunt cumque, nemo ea ad alias numquam quidem modi tempore voluptatem, nisi officia. Nemo voluptatem unde assumenda iure eos ad ipsa architecto, vel optio totam perferendis, quibusdam soluta omnis minima. Ex praesentium architecto natus mollitia quam tempora nulla molestias nam, doloremque fugiat perferendis quia consectetur voluptates ipsam, porro soluta eligendi esse ut laboriosam dignissimos quos sit. Fuga eius ipsa nisi quia maiores inventore ducimus rerum porro, maxime temporibus reiciendis voluptatem quos officia beatae esse expedita autem, sed cumque repellendus possimus quam, molestias error atque omnis. Magni totam esse possimus quos ipsam repellat architecto. Assumenda unde, necessitatibus aut maiores recusandae tempore itaque earum enim facere provident optio fugit minima, iusto reprehenderit, nobis saepe ducimus eum perferendis amet? Suscipit fugit voluptates amet incidunt rem pariatur veritatis, debitis vel ullam totam voluptas! Pariatur dolorum officia sed! Adipisci, laboriosam accusantium! Hic, eos? Ex accusamus voluptas reprehenderit nam, quia sed corporis a necessitatibus ratione natus facere tempore libero, ipsum explicabo, nemo debitis distinctio consequatur ducimus architecto cumque amet dolores facilis! Modi vitae dolor nihil inventore placeat distinctio ut facere, qui sapiente corporis veritatis architecto impedit tenetur voluptates nostrum amet sit quae accusantium expedita voluptatem dolorum, incidunt, exercitationem eos voluptate. Saepe eaque reprehenderit dolorum laborum repellendus veniam perferendis soluta itaque omnis architecto optio ratione officia, aliquam, impedit corporis repellat sequi quod dolores enim autem minima accusantium iure! Animi excepturi praesentium aperiam amet reprehenderit nemo eligendi? Nesciunt aliquam sequi iure magnam accusamus tempora rem natus cum, velit minima itaque? Fugiat, voluptas maxime?
-      </div>
-    </div>
   </div>
   <div style="margin-bottom: 2rem;"></div>
   <footer>
