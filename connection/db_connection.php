599d9f52adda572efcab5dd4d6d21514b6d9d76e
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-04-05 10:13:40 +0800

    update connection

---
+++ connection/db_connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$db_host = "localhost";
+$db_port = "5432";
+$db_name = "faculty";
+$db_user = "postgres";
+$db_password = "postgres";
+
+$conn = pg_connect("host=$db_host port=$db_port dbname=$db_name user=$db_user password=$db_password");
+
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
+}
+
+// echo "Connected successfully";
+
+pg_close($conn);
+?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('connection/db_connection.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <?php include('includes/header.php'); ?>
