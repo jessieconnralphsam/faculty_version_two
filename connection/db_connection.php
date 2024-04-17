0977dd703960fef557418306386b2e43a3a65d60
Jessie Conn Ralph <132178407+jessieconnralphsam@users.noreply.github.com>
2024-04-17 09:07:51 +0800

    update

--- connection/db_connection.php
+++ connection/db_connection.php
@@ -3,8 +3,8 @@
 $db_host = "localhost";
 $db_port = "5432";
 $db_name = "faculty";
-$db_user = "postgres";
-$db_password = "postgres";
+$db_user = "faculty_select";
+$db_password = '6M7$nT#pK*2j!uZ';
 
 $conn = pg_connect("host=$db_host port=$db_port dbname=$db_name user=$db_user password=$db_password");
 
